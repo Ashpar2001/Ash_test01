9e15fe6ffe9d33b58815f36c373b1d2d7c083cc2
Ashutosh <ashutoshpardeshi11@gmail.com>
2019-10-20 22:46:10 +0530

    Next commit

--- Test01/main.c
+++ Test01/main.c
@@ -4,5 +4,9 @@
 int main()
 {
     printf("Hello world!\n");
+    printf("1.First add the :- git add <File name>"
+           "\n2.Then commit it :- git commit -m \"Your comments\"\n"
+           "3.Create a new branch :- git branch <branch name>\n"
+           "4.To change the branch, use checkout :- ");
     return 0;
 }
